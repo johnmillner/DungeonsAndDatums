5ff44d5494b57c89edca40db22498ed67fbac72e
John Millner <millnerjohn@gmail.com>
2018-01-20 17:15:03 -0500

    bug fixing in progress

--- simulator-simple.py
+++ simulator-simple.py
@@ -77,7 +77,7 @@ def simulate_manyMonsters ( team1, team2 ):
 	else:
 		victor = "team 2"
 	
-	print "Victor is ", victor.name," and completed in ", counter, " turns!"
+	print "Victor is ", victor," and completed in ", counter, " turns!"
 	
 	
 def main():
