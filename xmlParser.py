23cd13e5ee6ee3eb8afbc05b090a228b70d5193a
Matthew Kurtz <matthewryankurtz@gmail.com>
2018-01-20 16:20:31 -0500

    insert_attacks update

--- xmlParser.py
+++ xmlParser.py
@@ -27,10 +27,10 @@ def insert_monster_attacks(name, attack):
 
 	cursor = conn.cursor()
 	cursor.execute("use dungeonsAndData;")
-	#cursor.execute("SELECT * from monster_attacks WHERE name LIKE '%s'" % (MySQLdb.escape_string(attack)))
-	#fore = cursor.fetchall()
-	#print "fore"
-	#print fore
+	cursor.execute("SELECT * from monster_attacks WHERE name LIKE '%s'" % (MySQLdb.escape_string(attack)))
+	fore = cursor.fetchall()
+	print "fore"
+	print fore
 	#if len(fore) is 0 :
 	cursor.execute(query)
 	conn.commit()
