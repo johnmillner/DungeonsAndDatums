56e591c5bc978fbd7813e224192379eadc98a4ef
John Millner <millnerjohn@gmail.com>
2018-01-20 18:57:18 -0500

    flavor comments

--- simulator-simple.py
+++ simulator-simple.py
@@ -87,8 +87,8 @@ def simulate_manyMonsters ( team1, team2 ):
 	
 	
 def main():
-	raw2 = ['Aarakocra', 'Aarakocra']
 	raw1 = ['Aarakocra', 'Aarakocra']
+	raw2 = ['Aarakocra', 'Aarakocra']
 
 	
 	#connect to database - yes i know how insecure this is - dont judge :'(
