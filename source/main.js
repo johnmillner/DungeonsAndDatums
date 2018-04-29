16ec614b86fe51449d4a976cca4e94aafff2f9ac
John Millner <millnerjohn@gmail.com>
2018-04-28 20:20:40 -0400

    testing http-vue-loader

--- source/main.js
+++ source/main.js
@@ -6,7 +6,7 @@ import { router } from "./router.js";
 // launches Vue instance - inserts into blank object called app
 export var vm = new Vue({
   el: '#app',
-  Router,
+  router,
   template: '<App/>',
   components: { 'app' : httpVueLoader('./components/app.vue') }
 })
