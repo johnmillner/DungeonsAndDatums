7fa4eae50e57cfb3b2b9517b38d6d00095e91f5c
John Millner <millnerjohn@gmail.com>
2018-04-28 20:02:51 -0400

    testing http-vue-loader

--- source/main.js
+++ source/main.js
@@ -2,12 +2,11 @@
 //  Created for:  Dungeons and Datums
 
 import Router from "./router.js";
-import App from "./components/app.vue";
 
 // launches Vue instance - inserts into blank object called app
 export var vm = new Vue({
   el: '#app',
   Router,
   template: '<App/>',
-  components: { App }
+  components: { 'app' : httpVueLoader('./components/app.vue') }
 })
